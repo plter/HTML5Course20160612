95296650b4b23d3e978b3777a8aaf92190d403df
梦宇 <xtiqin@163.com>
2016-06-24 16:57:45 +0800

    添加一些注释

--- 20160624/DragFileIn/app.js
+++ 20160624/DragFileIn/app.js
@@ -20,15 +20,18 @@
             if (file.type == "image/jpeg") {
 
                 var reader = new FileReader();
+
+                //读取文件完成以后执行该事件
                 reader.onload = function () {
                     if (currentImg) {
                         currentImg.parentNode.removeChild(currentImg);
                     }
 
                     currentImg = document.createElement("img");
-                    currentImg.src = reader.result;
+                    currentImg.src = reader.result;//获取读取结果
                     document.body.appendChild(currentImg);
                 };
+                //开始读取文件
                 reader.readAsDataURL(file);
             }
         }
