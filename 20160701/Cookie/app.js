1629e304e7ed3eb246c27d71f928cab298ad74c6
梦宇 <xtiqin@163.com>
2016-07-01 17:19:09 +0800

    用 ; 隔开cookie 键值对

--- 20160701/Cookie/app.js
+++ 20160701/Cookie/app.js
@@ -15,9 +15,6 @@
 
         console.log(document.cookie);
         this.readCookieData();
-
-        console.log(this._cookieKvs);
-
         this.addListeners();
     }
 
@@ -26,15 +23,15 @@
 
             this._cookieKvs[this._keyInput.value] = this._valueInput.value;
 
-            this.saveData();
+            document.cookie = this._keyInput.value + "=" + this._valueInput.value;
         }.bind(this);
     };
 
     Main.prototype.readCookieData = function () {
-        var str = document.cookie;//a=a&b=b&c=c
+        var str = document.cookie;
 
         if (str && str.length != 0) {
-            var kvs = str.split("&");
+            var kvs = str.split(";");
             for (var i = 0; i < kvs.length; i++) {
                 var kv = kvs[i].split("=");
                 this._cookieKvs[kv[0]] = kv[1];
@@ -46,16 +43,6 @@
             outputHtml += k + ":" + this._cookieKvs[k] + "<br>";
         }
         this._output.innerHTML = outputHtml;
-    };
-
-    Main.prototype.saveData = function () {
-        var str = "";
-        for (var k in this._cookieKvs) {
-            str += k + "=" + this._cookieKvs[k] + "&";
-        }
-        str = str.substr(0, str.length - 1);
-
-        document.cookie = str;
     };
 
     new Main();
