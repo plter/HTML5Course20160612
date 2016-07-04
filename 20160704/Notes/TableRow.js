fa092505f38eae9f5956896a65ae5a46a35c4921
梦宇 <xtiqin@163.com>
2016-07-04 10:59:20 +0800

    实现记帐作业示例

---
+++ 20160704/Notes/TableRow.js
@@ -0,0 +1,59 @@
+/**
+ * Created by plter on 7/4/16.
+ */
+
+window.ucai = window.ucai || {};
+
+(function () {
+
+    function TableRow(dbObject) {
+        this._dbObject = dbObject;
+
+        this._node = document.createElement("tr");
+        this._node.appendChild(this._createTd(dbObject.name));
+        this._node.appendChild(this._createTd(dbObject.date));
+        this._node.appendChild(this._createTd(dbObject.amount));
+        this._node.appendChild(this._createTd(dbObject.paied));
+
+        var td = this._createTd();
+        td.appendChild(this._createDeleteButton());
+        this._node.appendChild(td);
+    }
+
+    TableRow.prototype._createDeleteButton = function () {
+        var btn = document.createElement("button");
+        btn.innerHTML = "删除";
+        btn.onclick = function () {
+            if (this.onRequestDeleteRow) {
+                this.onRequestDeleteRow(this._dbObject);
+            }
+        }.bind(this);
+        return btn;
+    };
+
+    TableRow.prototype._createTd = function (content) {
+        // content = content ? content : "";
+        content = content || "";
+
+        var td = document.createElement("td");
+        td.innerHTML = content;
+        return td;
+    };
+
+    Object.defineProperty(TableRow.prototype, "node", {
+        get: function () {
+            return this._node;
+        }
+    });
+
+    Object.defineProperty(TableRow.prototype, "onRequestDeleteRow", {
+        get: function () {
+            return this._onRequestDeleteRow;
+        },
+        set: function (value) {
+            this._onRequestDeleteRow = value;
+        }
+    });
+
+    ucai.TableRow = TableRow;
+})();
---
+++ 20160704/Notes/main.js
@@ -0,0 +1,142 @@
+/**
+ * Created by plter on 7/4/16.
+ */
+
+(function () {
+
+    window.Config = {DB_NAME: "Notes", DB_VERSION: 1};
+
+
+    function Main() {
+        this.getElements();
+        this.addListeners();
+
+        this.connectDB(function () {
+            this.readData();
+        }.bind(this));
+    }
+
+    Main.prototype.getElements = function () {
+        this._dataContainer = document.getElementById("datacontainer");
+        this._msgContainer = document.getElementById("msgcontainer");
+        this._form = document.getElementById("form");
+    };
+
+    Main.prototype.addListeners = function () {
+        this._form.onsubmit = function (event) {
+            event.preventDefault();
+
+            var name = this._form['name'];
+            var date = this._form["date"];
+            var amount = this._form["amount"];
+            var paied = this._form["paied"];
+
+            this.saveData({name: name.value, date: date.value, amount: amount.value, paied: paied.value});
+        }.bind(this)
+    };
+
+    Main.prototype.clearFormFields = function () {
+        this._form["name"].value = "";
+        this._form["amount"].value = "";
+        this._form["paied"].value = "";
+    };
+
+    Main.prototype.saveData = function (data) {
+        var trans = this._db.transaction("notes", "readwrite");
+        var os = trans.objectStore("notes");
+        var req = os.put(data);
+        req.onsuccess = function () {
+            this._msgContainer.innerHTML = "保存数据成功";
+            this.clearFormFields();
+
+            this.readData();
+        }.bind(this);
+        req.onerror = function () {
+            this._msgContainer.innerHTML = "保存数据失败";
+        }.bind(this);
+    };
+
+
+    Main.prototype.readData = function () {
+        var trans = this._db.transaction("notes");
+        var os = trans.objectStore("notes");
+        var req = os.getAll();
+        req.onerror = function () {
+            this._msgContainer.innerHTML = "读取数据失败";
+        }.bind(this);
+        req.onsuccess = function () {
+            var data = event.target.result;
+            console.log(data);
+
+            this.showData(data);
+        }.bind(this)
+    };
+
+    Main.prototype.showData = function (data) {
+        if (this._table) {
+            this._table.parentNode.removeChild(this._table);
+        }
+
+        this._table = document.createElement("table");
+        var _tr = document.createElement("tr");
+        this._table.appendChild(_tr);
+        _tr.appendChild(this.createElementAndContent("td", "名字"));
+        _tr.appendChild(this.createElementAndContent("td", "日期"));
+        _tr.appendChild(this.createElementAndContent("td", "欠钱数额"));
+        _tr.appendChild(this.createElementAndContent("td", "是否还清"));
+
+        for (var i = 0; i < data.length; i++) {
+
+            var tr = new ucai.TableRow(data[i]);
+            tr.onRequestDeleteRow = this.requestDeleteRowHandler.bind(this);
+            this._table.appendChild(tr.node);
+        }
+
+        this._dataContainer.appendChild(this._table);
+    };
+
+    Main.prototype.requestDeleteRowHandler = function (dbObject) {
+        var trans = this._db.transaction("notes", "readwrite");
+        var os = trans.objectStore("notes");
+        var req = os.delete(dbObject.id);
+        req.onerror = function () {
+            this._msgContainer.innerHTML = "删除数据失败";
+        }.bind(this);
+        req.onsuccess = function () {
+            this._msgContainer.innerHTML = "删除数据成功";
+
+            this.readData();
+        }.bind(this);
+    };
+
+    Main.prototype.createElementAndContent = function (tagName, content) {
+        var tag = document.createElement(tagName);
+        tag.innerHTML = content;
+        return tag;
+    };
+
+    Main.prototype.connectDB = function (sucCallback) {
+        var req = indexedDB.open(Config.DB_NAME, Config.DB_VERSION);
+        req.onsuccess = function (event) {
+            this._db = event.target.result;
+
+            sucCallback();
+        }.bind(this);
+        req.onerror = function (event) {
+            console.error(event);
+        };
+        req.onupgradeneeded = function (event) {
+
+            var db = event.target.result;
+
+            var os = db.createObjectStore("notes", {keyPath: "id", autoIncrement: true});
+            os.createIndex("name", "name");
+            os.createIndex("date", "date");
+            os.createIndex("amount", "amount");
+            os.createIndex("paied", "paied");
+        };
+    };
+
+
+    new Main();
+})();
