88d25ea0aa7b2b936374edab01e902ad4d364a3c
梦宇 <xtiqin@163.com>
2016-07-09 13:01:48 +0800

    上传示例

---
+++ 20160709/swipe/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by plter on 7/9/16.
+ */
+
+
+(function () {
+
+    // $(document).on("touchstart", function (event) {
+    //     console.log(event);
+    // });
+    // $(document).on("touchend", function (event) {
+    //     console.log(event);
+    // });
+    // $(document).on("touchmove", function (event) {
+    //     console.log(event.originalEvent.touches);
+    // });
+
+    // $(document).on("scrollstop", function () {
+    //     console.log("scrollstop");
+    // });
+
+    // console.log(window.innerWidth, window.innerHeight);
+})();
